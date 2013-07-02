7e2e487d30c10076d9ff75dfd9f296e0f6d2bade
YUChoe <scr4free@gmail.com>
2013-07-02 12:26:00 +0900

    result5 로 output file 변경. mismatch 의 경우 추정하도록 로직 추가

--- mk_result4.py
+++ mk_result4.py
@@ -30,15 +30,17 @@ port_data = {}
 def in_pok(ipaddr) :
     tip = ipaddr.split('.')
     if tip[0] in ['10', '172','192','190','100','61'] : 
+        if tip[0] == '61' and (not (tip[1] == '33' and tip[2] == '233')) :
+                return False
         return True
     else : 
         return False
-
 
 for filename in source_files_ls.split('\n') :
     _time = time.localtime()
     print "processing", filename, '%02d:%02d:%02d' % (_time[3], _time[4], _time[5])
     fp = file(filename, 'r')
+    
     for line in fp.readlines() :
         line = line[:-1]
         if line == '' : continue
@@ -47,11 +49,15 @@ for filename in source_files_ls.split('\n') :
             print 'Error:', line
             continue
         if r[8] == 'S-C' : 
+            # 2013-06-08 23:59:28,61.33.233.29,172.20.152.7,80,3348,538083268,4,6,S-C
             continue
             #server_ip = r[1]
             #listen_port = r[4]
         elif r[8] == 'C-S' : 
+            # 2013-06-08 23:59:28,172.25.0.213,172.29.0.1,58900,22,-1308739514,24,6,C-S
+            client_ip = r[1]
             server_ip = r[2]
+            req_port = r[3]
             listen_port = r[4]
         else :
             continue
@@ -68,15 +74,23 @@ for filename in source_files_ls.split('\n') :
             else :
                 #print "passed", server_ip, listen_port
                 continue
+        elif port_data.has_key(client_ip) :
+            # mismatch ? 
+            continue
+            #_ports = port_data[client_ip]
+            #if req_port not in _ports : 
+            #    _ports.append(req_port)
+
         else : 
             _ports = [listen_port]
             port_data[server_ip] = _ports
             #print "new", server_ip, listen_port
     print "finished -", len(port_data)
     fp.close()
+    break
 
 total_rows = len(port_data)
-fp = file('TG_result4.csv', 'w')
+fp = file('TG_result5.csv', 'w')
 idxcount = 0
 for server_ip in port_data : 
     listen_ports = port_data[server_ip]
