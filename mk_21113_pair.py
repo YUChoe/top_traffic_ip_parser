26886837571d6bf0328a80b1f9d9fd2ef7b9f0b5
YUChoe <scr4free@gmail.com>
2013-07-05 10:16:25 +0900

    output 규칙 수정

--- mk_21113_pair.py
+++ mk_21113_pair.py
@@ -5,32 +5,30 @@ import sys
 cs_fp = file('C-S_only.dat', 'r')
 
 sip = ['172.16.20.67','172.16.20.68','172.16.20.69']
-data = []
+data = {}
 
 idxcnt = 0
 for l in cs_fp.xreadlines():
   l = l.rstrip().split(',')
 
+  """
   if (l[3] != '21113' and l[4] != '21113') : 
     idxcnt += 1
     continue
+  """
 
-  if (l[3] == '21113' and l[2] in sip) or (l[4] == '21113' and l[1] in sip) :
-    pass
-  elif l[1] not in sip :
-    sip.append(l[2])
-    data.append(l)
-  elif l[2] not in sip : 
-    sip.append(l[1])
-    data.append(l)
+  if (l[4] == '21113' and l[1] in sip) :
+    if l[2] not in data : 
+      n_row = '%s,%s,%s' % (l[1], l[2], '21113')
+      data[l[1]] = n_row
 
   idxcnt += 1
 
   if idxcnt % 10000 == 0 : 
-    print '\r', idxcnt,'/90360496', len(sip),
+    print '\r', idxcnt,'/90360496', int(idxcnt*100/90360496),'%', len(data), 
     sys.stdout.flush()
 
-print '\r', idxcnt, '/90360496', len(sip)
+print '\r', idxcnt, '/90360496 100 %', len(data)
 
 
 
@@ -40,7 +38,7 @@ cs_fp.close()
 fp = file('TG_result7_21113.csv', 'w')
 #for ip in sip : 
 #  fp.write( ip + '\n')
-for l in data : 
-    fp.write(','.join(l) + '\n')
+for _k in data : 
+    fp.write( data[_k] + '\n')
 fp.close()
 
